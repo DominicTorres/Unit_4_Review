5d001836710b0433cb6a504610edc0600e78fd09
Dominic <dtorres243@west-mec.org>
2020-03-10 18:25:17 -0700

    it works

--- jpf_hitori.js
+++ jpf_hitori.js
@@ -1,13 +1,10 @@
 "use strict";
-
 /*
    New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
    Tutorial 11
    Review Assignment
-
    Author: Dominic Torres
-   Date: 3/9/20  
-
+   Date: 03/9/20
    Global Variables
    ================
    
@@ -16,7 +13,6 @@
       
    Function List
    =============
-
    startUp()
       Run when the web page is loaded; displays puzzle 1
       and loads the event handlers for the web page buttons.
@@ -24,12 +20,10 @@
    setupPuzzle()
       Sets up a new puzzle, adding the event handlers for
       every puzzle cell.      
-
    switchPuzzle(e)
       Swaps one puzzle for another based on the button being clicked
       by the user. Confirms the change before swapping in the
       new puzzle.
-
    findErrors()
       Highlights the errors in the Hitori puzzle in a red font.
       
@@ -39,15 +33,12 @@
    checkSolution()
       Checks the current user's puzzle to verify whether it contains
       the complete and correct solution.
-
    drawHitori(numbers, blocks, rating)
       Returns a text string of the HTML code to
       display a Hitori puzzle table based on the contents of
       the numbers, blocks, and rating parameters.
-	
+   
 */
-
-//Global Variables
 var allCells;
 window.onload = startUp;
 function startUp(){
@@ -137,23 +128,16 @@ function findErrors(){
          }
       }, 500);
 }
-
-
-
-
+         
 /* ================================================================= */
-
 function checkSolution() {
    /* Set the initial solved state of the puzzle to true */
    var solved = true;
-
    /* Loop through the puzzle cells, exiting when an incorrect
       cell is found, setting the solved variable to false */
-
    for (var i = 0; i < allCells.length; i++) {
       var cellColor = allCells[i].style.backgroundColor;
       var cellClass = allCells[i].className;
-
       /* A cell is incorrect if it is in the block class and is not black
          or in the circle class and is not white */
       if ( (cellClass == "blocks" && cellColor !== "black") || 
@@ -162,11 +146,9 @@ function checkSolution() {
          break;
       }
    }
-
    /* If solved is still true after the loop, display an alert box */
    if (solved) alert("Congratulations! You solved the puzzle!");
 }
-
 function showSolution () {
    for (var i = 0; i < allCells.length; i++) {
       allCells[i].style.color = "";
@@ -174,45 +156,34 @@ function showSolution () {
       allCells[i].style.borderRadius = "";
    };   
 }
-
 function drawHitori(numbers, blocks, rating) {
-
    /* Initial HTML String for the Hitori Puzzle */
    var htmlString = "";
-
    /* numbers is a multidimensional array containing the
       Hitori numbers; blocks is a corresponding 
       multidimensional array containing the location of the
       blocks which are indicated by the # character.
       Non-blocking cells are indicated by a blank character.
   */
-
    /* Create a Web table with the id, hitoriGrid, containing
       the numeric values. Blocks cells have the class name,
       blocks. Non-blocking cells have the class name, circles
   */
-
    var totalRows = numbers.length;
    var totalCols = numbers[0].length;
    htmlString = "<table id='hitoriGrid'>";
    htmlString += "<caption>" + rating + "</caption>";
    
-
    for (var i = 0; i < totalRows; i++) {
       htmlString += "<tr>";
-
       for (var j = 0; j < totalCols; j++) {
          if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>"
          else htmlString += "<td class='circles'>";
-
          htmlString += numbers[i][j];
          htmlString +="</td>";
       }
-
       htmlString += "</tr>";
    }
-
    htmlString += "</table>";
-
    return htmlString;
 }
